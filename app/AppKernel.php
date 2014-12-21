3f8303ad4184007f9e18555024f2e005ae54a729
Sergio Gómez <sergio@uco.es>
2014-12-21 23:12:57 +0100

    Actualizadas las librerías. Se elimina LadyBugBundle, por tener soporte de debug de variables en symfony2.6

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -33,7 +33,6 @@ class AppKernel extends Kernel
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
             $bundles[] = new Elao\WebProfilerExtraBundle\WebProfilerExtraBundle();
             $bundles[] = new h4cc\AliceFixturesBundle\h4ccAliceFixturesBundle();
-            $bundles[] = new RaulFraile\Bundle\LadybugBundle\RaulFraileLadybugBundle();
         }
 
         return $bundles;
