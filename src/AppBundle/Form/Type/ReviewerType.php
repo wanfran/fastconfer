a9601ae9e1e6d9291e9467b1c9878ea8d49a10ce
Francisco Rodriguez Bergillos <i02robef@uco.es>
2015-03-24 20:07:16 +0100

    Relleno de datos con Alice

--- src/AppBundle/Form/Type/ReviewerType.php
+++ src/AppBundle/Form/Type/ReviewerType.php
@@ -8,7 +8,37 @@
 
 namespace AppBundle\Form\Type;
 
+use AppBundle\Entity\ReviewComments;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class ReviewerType {
+class ReviewerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('comment','textarea')
+            ->add('state', 'choice', array(
+                'choices' => array('accepted' => 'Accepted', 'accepted with suggestions' => 'Accepted with suggestions',
+                    'rejected' => 'Rejected'),
+                'preferred_choices' => array('accepted'),
+            ));
+//            ->add('save', 'submit', array('label' => 'Submit'));
+
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\ReviewComments'
+        ));
+    }
+    public function getName()
+    {
+        return 'ReviewComments';
+    }
+
 
 }
