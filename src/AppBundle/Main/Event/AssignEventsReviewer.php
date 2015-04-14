1cfbfd374519fdcac9225294ad5d698d734fafc1
Francisco Rodriguez Bergillos <i02robef@uco.es>
2015-04-14 23:07:28 +0200

    clase eliminada

---
+++ src/AppBundle/Main/Event/AssignEventsReviewer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fran
+ * Date: 14/04/15
+ * Time: 19:26
+ */
+
+class AssignEventsReviewer {
+
+}
---
+++ src/AppBundle/Main/EventListener/AssignReviewerSubscriber.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fran
+ * Date: 14/04/15
+ * Time: 19:37
+ */
+
+class AssignReviewerSubscriber {
+
+}
