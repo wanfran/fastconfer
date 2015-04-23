a2e02584154b981b83086a8396cb0c9310197b94
Francisco Rodriguez Bergillos <i02robef@uco.es>
2015-04-23 18:54:13 +0200

    Archivos creados con spBower

---
+++ web/js/419301c_fastconfer_7.js
@@ -0,0 +1,7 @@
+$(function() {
+  $(document.body).on('appear', '.conference-card', function(e, $affected) {
+    // add class called “appeared” for each appeared element
+    $(this).addClass('appeared');
+  });
+  $('.conference-card').appear({force_process: true});
+});
