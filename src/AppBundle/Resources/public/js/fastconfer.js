a23e02a756996a0bbf9f3f1b6741088285e8c54f
Francisco Rodriguez Bergillos <i02robef@uco.es>
2015-04-26 20:16:21 +0200

    Estilos propios

--- src/AppBundle/Resources/public/js/fastconfer.js
+++ src/AppBundle/Resources/public/js/fastconfer.js
@@ -1,7 +1,11 @@
-$(function() {
-  $(document.body).on('appear', '.conference-card', function(e, $affected) {
-    // add class called “appeared” for each appeared element
-    $(this).addClass('appeared');
-  });
-  $('.conference-card').appear({force_process: true});
+$(document).ready(function(){
+    $(function() {
+        $(document.body).on('appear', '.conference-card', function(e, $affected) {
+            // add class called “appeared” for each appeared element
+            $(this).addClass('appeared');
+        });
+        $('.conference-card').appear({force_process: true});
+    });
+
+    $('select').select2();
 });
