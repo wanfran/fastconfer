0cc0ce1c0c56ec149ea2ccc7a5b57564330f1b0e
Francisco Rodriguez Bergillos <i02robef@uco.es>
2015-04-26 20:14:02 +0200

    Archivos eleminados

--- web/js/419301c_fastconfer_7.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  $(document.body).on('appear', '.conference-card', function(e, $affected) {
-    // add class called “appeared” for each appeared element
-    $(this).addClass('appeared');
-  });
-  $('.conference-card').appear({force_process: true});
-});
