98b1181c9a75962c4a8550d5cb614f9d517c155a
Francisco Rodriguez Bergillos <i02robef@uco.es>
2015-05-05 19:50:52 +0200

    Datos de alice

--- src/AppBundle/DataFixtures/Alice/TaskListSet.php
+++ src/AppBundle/DataFixtures/Alice/TaskListSet.php
@@ -12,6 +12,7 @@ $set->addFile(__DIR__.'/users.yml', 'yaml');
 $set->addFile(__DIR__.'/conferences.yml', 'yaml');
 $set->addFile(__DIR__.'/inscription.yml', 'yaml');
 $set->addFile(__DIR__.'/articles.yml', 'yaml');
+$set->addFile(__DIR__.'/author.yml', 'yaml');
 $set->addFile(__DIR__.'/articlesReview.yml', 'yaml');
 $set->addFile(__DIR__.'/reviewer.yml', 'yaml');
 $set->addFile(__DIR__.'/reviewComments.yml', 'yaml');
