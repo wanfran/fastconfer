cc8ef44d095689d2818f332e914023f8a04dfe34
Francisco Rodriguez Bergillos <i02robef@uco.es>
2015-06-02 20:29:26 +0200

    archivo renombrado por logo.ico

---
+++ src/AppBundle/Form/Type/ProfileFormHandler.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fran
+ * Date: 2/06/15
+ * Time: 19:33
+ */
+
+namespace AppBundle\Form\Type;
+
+
+class ProfileFormHandler {
+
+}
---
+++ src/AppBundle/Form/Type/ProfileType.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fran
+ * Date: 2/06/15
+ * Time: 19:30
+ */
