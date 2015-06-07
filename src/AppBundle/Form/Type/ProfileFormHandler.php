b5fdcc3c67afa81f0626501c4a20666e76434bc5
Francisco Rodriguez Bergillos <i02robef@uco.es>
2015-06-07 18:17:58 +0200

    cambios

--- src/AppBundle/Form/Type/ProfileFormHandler.php
+++ src/AppBundle/Form/Type/ProfileFormHandler.php
@@ -7,8 +7,33 @@
  */
 
 namespace AppBundle\Form\Type;
+use FOS\UserBundle\Model\UserInterface;
+use FOS\UserBundle\Form\Handler\ProfileFormHandler as BaseHandler;
 
+class ProfileFormHandler extends BaseHandler
+{
 
-class ProfileFormHandler {
+    public function process(UserInterface $user)
+    {
+        return parent::process($user);
 
+        $this->form->setData($user);
+        if ('POST' == $this->request->getMethod()) {
+            $this->form->bindRequest($this->request);
+
+            if ($this->form->isValid()) {
+                $nombreArchivoFoto = uniqid().$user->getId() . '-' . $user->getUsername() . '-foto-perfil.jpg';
+                $user->upload($nombreArchivoFoto);
+                $this->onSuccess($user);
+                return true;
+            }
+            $this->userManager->reloadUser($user);
+        }
+        return false;
+    }
+
+    protected function onSuccess(UserInterface $user)
+    {
+        $this->userManager->updateUser($user);
+    }
 }
--- src/AppBundle/Form/Type/ProfileType.php
+++ src/AppBundle/Form/Type/ProfileType.php
@@ -5,3 +5,23 @@
  * Date: 2/06/15
  * Time: 19:30
  */
+
+namespace AppBundle\Form\Type;
+use FOS\UserBundle\Form\Type\ProfileFormType as BaseType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+
+class ProfileType extends BaseType
+{
+    public function buildUserForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildUserForm($builder, $options);
+
+        $builder->add('organization');
+    }
+
+    public function getName()
+    {
+        return 'fastconfer_user_profile';
+    }
+}
