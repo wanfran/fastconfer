0aa430236b5613b0185217f14abe5f4f0a94edb4
Francisco Rodriguez Bergillos <i02robef@uco.es>
2015-06-22 18:46:13 +0200

    Añadidos campos al Perfil

--- src/AppBundle/Form/Handler/ProfileFormHandler.php
+++ src/AppBundle/Form/Handler/ProfileFormHandler.php
@@ -11,16 +11,35 @@ namespace AppBundle\Form\Handler;
 use FOS\UserBundle\Model\UserManagerInterface;
 use Sonata\UserBundle\Form\Handler\ProfileFormHandler as BaseHandler;
 use Symfony\Component\Form\Form;
-use Symfony\Component\Form\FormBuilder;
+
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Class ProfileFormHandler
+ * @package AppBundle\Form\Handler
+ */
 class ProfileFormHandler extends BaseHandler
 {
     public function __construct(Form $form, Request $request, UserManagerInterface $userManager)
     {
-        $form->add(
-            'organization'
-        );
+        // borrando campos no necesarios
+        $form->remove('gender');
+        $form->remove('website');
+        $form->remove('biography');
+        $form->remove('timezone');
+        $form->remove('phone');
+        $form->remove('locale');
+        $form->remove('dateOfBirth');
+
+        // cargando nuevos campos
+        $form->add('username',null, array(
+            'label' => 'Name of User'));
+        $form->add('email');
+        $form->add('organization');
+//        $form->add('gravatar', 'file', array(
+//        'mapped' => false,
+//        'attr' => ['class' => 'filestyle','data-buttonBefore'=> 'true', 'data-buttonText' => 'Choose file' ]
+//    ));
         parent::__construct($form, $request, $userManager);
     }
 
